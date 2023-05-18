d26b546a1f5527b9e884cb45ad256d601f2f5229
sharonvijay <94788632+sharonvijay@users.noreply.github.com>
2023-05-18 23:28:56 +0530

    update mongodb

--- server/index.js
+++ server/index.js
@@ -1,23 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
-
-
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use('/posts',postRoutes);
+app.use("/posts", postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://sharon:sharon@cluster0.kcqjlij.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL =
+  "mongodb+srv://sharonvijay2003:sharonvijay2003@mern.72x3pbl.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log(`server running on port : ${PORT}`)))
-    .catch((error) => console.log(`${error} did not connect`));
-
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running on port : ${PORT}`))
+  )
+  .catch((error) => console.log(`${error} did not connect`));
